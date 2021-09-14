6516aaffaa53658668d09748b96e3a47c3a642cb
Alessandro Gums <90655009+alessandrogums@users.noreply.github.com>
2021-09-14 09:29:34 -0300

    Update and rename Exercicio.10_listaPyBr.py to Exercicio.10e11_listaPyBr.py

--- Exercicio.10e11_listaPyBr.py
+++ Exercicio.10e11_listaPyBr.py
@@ -10,3 +10,17 @@ else:
     print('os números entre o número 2 e o número 1 são:')
     for c in range(num2+1,num1):
         print(c,end=' ')
+ 
+#Altere o programa anterior para mostrar no final a soma dos números.
+
+ num1 = int(input('digite o primeiro número inteiro:'))
+num2 = int(input('digite o segundo número inteiro:'))
+soma = 0
+if num2 > num1:
+    for c in range(num1 + 1, num2):
+        soma += c
+    print(f'a soma dos números inteiros entre o {num1} e o {num2} equivale a {soma}')
+else:
+    for c in range(num2 + 1, num1):
+        soma += c
+    print(f'a soma dos números inteiros entre o {num2} e o {num1} equivale a {soma}')
