f8b8977873b62a4cf0900d68f9b32a46ba870767
Alessandro Gums <90655009+alessandrogums@users.noreply.github.com>
2021-09-14 09:39:10 -0300

    Create Exercicio.20_listaPyBr.py

---
+++ Exercicio.20_listaPyBr.py
@@ -0,0 +1,12 @@
+#Altere o programa de cálculo do fatorial, permitindo ao usuário calcular o fatorial várias vezes e limitando o fatorial a números inteiros positivos e menores que 16.
+
+while True:
+#     num=int(input('digite o número que você quer transformar em fatorial:'))
+#     fatorial = 1
+#     while not 16>num>0:
+#         num=int(input('digite o número novamente, precisando ser positivo e menor do que 16:'))
+#     print(f'o resultado do fatorial de {num}!=',end='')
+#     while not num==1:
+#         fatorial*=num
+#         num-=1
+#     print(fatorial,end='\n')
